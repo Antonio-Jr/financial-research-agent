1bd38dbc82c59cb05ee4ea08e7fb847cdc027213
Antonio-Jr <antonio.jr.ssouza@gmail.com>
2026-03-01 17:28:21 -0300

    fat: updating env and config to get the email sender

--- src/config.py
+++ src/config.py
@@ -7,10 +7,11 @@ class Settings(BaseSettings):
   LLM_BASE_URL: str = Field(default=...)
   LLM_MODEL_NAME: str = Field(default=...)
   SENDGRID_API_KEY: str = Field(default=...)
+  SENDER_EMAIL_ADDRESS: str = Field(default=...)
 
   model_config = SettingsConfigDict(env_file=".env")
 
-@lru_cache
+@lru_cache(maxsize=1)
 def get_settings() -> Settings:
   return Settings()
 
