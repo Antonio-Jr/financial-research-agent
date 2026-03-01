0f4e2e9372edb38e71e63ac1afeb4f661deb80e9
Antonio-Jr <antonio.jr.ssouza@gmail.com>
2026-03-01 15:13:03 -0300

    feat: Adding search_planner runner

---
+++ src/runners/search_planner.py
@@ -0,0 +1,16 @@
+
+from agents import Runner
+from core.base_runner import BaseRunner
+from src.models.search_planner import SearchPlan
+from src.agents.search_planner import SearchPlannerAgent
+
+
+class SearchPlannerRunner(BaseRunner[SearchPlannerAgent, SearchPlan]):
+  @classmethod
+  async def run(cls, query: str):
+    """Use planner_agent to generate a structured list of web searches for a given research query."""
+    print("Planning for searches...")
+    result = await cls.execute(query)
+    print(f"Will perform {len(result.searches)} searches")
+
+    return result
