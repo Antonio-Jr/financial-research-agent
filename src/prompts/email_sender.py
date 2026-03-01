93b8b2bf40b77122cfeeee7c747c532b5768ab87
Antonio-Jr <antonio.jr.ssouza@gmail.com>
2026-03-01 13:21:40 -0300

    feat: Adding prompts definition

---
+++ src/prompts/email_sender.py
@@ -0,0 +1,4 @@
+# Email Agent
+INSTRUCTIONS = """You are able to send a nicely formatted HTML email based on detailed report.
+You will be provided with a detailed report. You should use your tool to send one email, providing the report
+converted into clean, well presented HTML with an appropriate subject line."""
